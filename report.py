764fda4b55ff38d40e2626bba9ea9b729be68ea8
jmontee <ijmontee@gmail.com>
2022-01-27 16:59:09 -0800

    added half of part 4 completion and did some more updates to is_valid function

--- report.py
+++ report.py
@@ -1,6 +1,33 @@
 import scraper
 from bs4 import BeautifulSoup
 from utils.response import Response
+from urllib.parse import urlparse
+import re
+
+
+
+# helper function to aid with finding the 50 most common words
+def tokenize(text: str) -> list:
+    token_list = []
+
+    # use regex on apostrophes to unite contractions
+    p = re.compile('[\']')
+    text = p.sub('', text)
+
+    # use regex on any non-alphabetic chars
+    p = re.compile('[^a-zA-Z]')
+    # USE THIS LINE TO INCLUDE NUMBERS 
+    #p = re.compile('[^a-zA-Z0-9]')
+
+    # replace them with a space
+    text = p.sub(' ', text)
+
+    # add each word to the token list
+    for word in text.split():
+        token_list.append(word)
+    
+    return token_list
+
 
 # this class is intended to preserve information needed between analyses on different
 # pages. 
@@ -10,6 +37,10 @@ class Report():
         # (HTML markup doesn’t count as words)
         self.longest_page = ('', 0)
 
+        #Question 4 how mainy subdomains did you find and the number of unique pages
+        self.unique_subdomains = {}
+
+
     # accessor to retrieve the longest page's word count. this allows for easy
     # comparison between a current page's word count and the previous longest page
     def get_page_word_count(self) -> int:
@@ -18,6 +49,22 @@ class Report():
     # accessor to retrieve the longest page in string format
     def get_page_url(self) -> str:
         return self.longest_page[0]
+
+
+
+    #function to add the unqiue subdomain found
+    def add_subdomain(self, url):
+        
+        #checks to make sure the url is valid first
+        if(scraper.is_valid(url)):
+            parsed = urlparse(url)
+            #print(url)
+            #checks to make sure the url was not already looked at and that it is a subdomain of .ics.uci.edu
+            if parsed.netloc not in self.unique_subdomains.keys() and re.search('\.ics\.uci\.edu', url):
+                print(parsed.netloc)
+                self.unique_subdomains.update({parsed.netloc: 0})
+        
+ 
 
     # this function expects the response received by the scraper function. 
     # this function will use BeautifulSoup to get just the text content
@@ -44,4 +91,8 @@ class Report():
         two = 'The longest page is ' + self.longest_page[0] + ' with ' + str(self.longest_page[1]) + ' words.\n'
         three = ''
         four = ''
+        # #print(self.unique_subdomains.keys())
+        # for page in self.unique_subdomains.keys():
+        #     print(page)
+    
         return (one + two + three + four)
--- scraper.py
+++ scraper.py
@@ -1,16 +1,23 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
 from bs4 import BeautifulSoup as BS
-from report import Report
+from report import Report, tokenize
 from utils.response import Response
 
-def scraper(url, resp, report: Report):
-    links = extract_next_links(url, resp)
 
+
+
+
+def scraper(url, resp, report: Report):
+   
+    links = extract_next_links(url, resp)
     # count the total number of words in this page and save the total if it's
     # higher than the previous max
     report.count_total_page_words(url, resp)
 
+    #adds the domain if it is unique 
+    report.add_subdomain(url)
+    
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp: Response):
@@ -24,18 +31,34 @@ def extract_next_links(url, resp: Response):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     found_urls = []
-    #status 404 going in. 
+    
+
+    #Checks to make sure we have a the status is 200 before looking at the urls
     if(resp.status == 200):
+        
+        #parses the html of the page
         info = BS(resp.raw_response.content, 'html.parser')
+        #gets all the a tags of the html
         all_urls = info.find_all('a')
+
+        #goes through all the a tags found to pull out the urls from the href
         for found_url in all_urls:
             url_to_add = found_url.get('href')
-            if(url_to_add != url):
-                found_urls.append(url_to_add)
+            #checks to make sure that the link we found is not the same as the page url and that the url was not found before 
+            if(url_to_add != url and url_to_add not in found_urls):
+                #adds the url to the list while also taking out the fragment. 
+                found_urls.append(urldefrag(url_to_add).url)
         return found_urls
         
 
     return list()
+
+#helper function to check to see if we are looking in one of the valid domains we were given. 
+def in_valid_domain(url, parsed):
+    if re.search('\.ics\.uci\.edu|\.cs\.uci\.edu|\.informatics\.uci\.edu|\.stat\.uci\.edu|today.uci.edu/department/information_computer_sciences', parsed.netloc):
+        return True
+    else:
+        return False
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -45,7 +68,7 @@ def is_valid(url):
         parsed = urlparse(url)
         #or parsed.hostname not in set(["www.ics.uci.edu","www.cs.uci.edu/","www.informatics.uci.edu/", "www.stat.uci.edu/" ])
         #looks throught to make sure the domain is one of the ones we are supposed to be in. Will also filter out comment sections so that they are not valid urls to look at. 
-        if parsed.scheme not in set(["http", "https"]) or not re.search('ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu', parsed.netloc) or re.match('comment|respond', parsed.fragment):
+        if parsed.scheme not in set(["http", "https"]) or not in_valid_domain(url, parsed) or re.match('replytocom', parsed.query) or re.search('event', url):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
